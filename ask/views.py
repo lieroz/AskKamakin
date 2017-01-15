b8980e327db53b4fa083a6663bf00ed3bec8d4d1
lieroz <a.kamakin@icloud.com>
2017-01-16 01:45:56 +0300

    Implemented decorator for tags.

--- ask/views.py
+++ ask/views.py
@@ -4,11 +4,21 @@ from django.contrib import auth
 from django.http import HttpResponseRedirect
 
 from ask.forms import LoginForm, SignupForm, SettingsForm, QuestionForm, AnswerForm
-from ask.models import Question
+from ask.models import Question, Tag
 from ask.ajax_features import login_required_ajax
 
 
-def pagination(request, objects, objects_count, html_page):
+def tags_decorator(func):
+    def decorator(request, *args, **kwargs):
+        tags = Tag.objects.count_popular()
+
+        return func(request, tags=tags, *args, **kwargs)
+
+    return decorator
+
+
+@tags_decorator
+def pagination(request, objects, objects_count, html_page, **kwargs):
     paginator = Paginator(objects, objects_count)
     page = request.GET.get('page')
 
@@ -19,10 +29,14 @@ def pagination(request, objects, objects_count, html_page):
     except EmptyPage:
         list_objects = paginator.page(paginator.num_pages)
 
-    return render(request, html_page, {'objects': list_objects})
+    return render(request, html_page, {
+        'objects': list_objects,
+        'tags': kwargs['tags'],
+    })
 
 
-def login(request):
+@tags_decorator
+def login(request, **kwargs):
     redirect = request.GET.get('continue', '/')
 
     if request.user.is_authenticated():
@@ -38,12 +52,14 @@ def login(request):
     else:
         form = LoginForm()
 
-    return render(request, 'template_form.html', {
+    return render(request, 'login.html', {
         'form': form,
+        'tags': kwargs['tags'],
     })
 
 
-def signup(request):
+@tags_decorator
+def signup(request, **kwargs):
     if request.user.is_authenticated():
         return HttpResponseRedirect('/')
 
@@ -58,20 +74,22 @@ def signup(request):
     else:
         form = SignupForm()
 
-    return render(request, 'template_form.html', {
+    return render(request, 'signup.html', {
         'form': form,
+        'tags': kwargs['tags'],
     })
 
 
 @login_required_ajax
-def logout(request):
+def logout(request, **kwargs):
     redirect = request.GET.get('continue', '/')
     auth.logout(request)
     return HttpResponseRedirect(redirect)
 
 
+@tags_decorator
 @login_required_ajax
-def settings(request):
+def settings(request, **kwargs):
     if not request.user.is_authenticated():
         return HttpResponseRedirect('/')
 
@@ -84,13 +102,15 @@ def settings(request):
     else:
         form = SettingsForm(request.user)
 
-    return render(request, 'template_form.html', {
+    return render(request, 'settings.html', {
         'form': form,
+        'tags': kwargs['tags'],
     })
 
 
+@tags_decorator
 @login_required_ajax
-def question_add_form(request):
+def question_add_form(request, **kwargs):
     if request.method == 'POST':
         form = QuestionForm(request.user, request.POST)
 
@@ -100,13 +120,15 @@ def question_add_form(request):
     else:
         form = QuestionForm()
 
-    return render(request, 'template_form.html', {
+    return render(request, 'ask.html', {
         'form': form,
+        'tags': kwargs['tags'],
     })
 
 
+@tags_decorator
 @login_required_ajax
-def question_view(request, question_id):
+def single_question(request, question_id, **kwargs):
     question = Question.objects.get(id=question_id)
 
     if request.method == 'POST':
@@ -118,14 +140,14 @@ def question_view(request, question_id):
     else:
         form = AnswerForm(initial={'question_id': question_id})
 
-    return render(request, 'template_form.html', {
+    return render(request, 'question.html', {
         'question': question,
         'is_preview': False,
         'form': form,
+        'tags': kwargs['tags'],
     })
-
 
 
 def main_page(request):
     questions = Question.objects.all()
-    return pagination(request, questions, 5, 'main_page.html')
+    return pagination(request, questions, 5, 'base.html')
